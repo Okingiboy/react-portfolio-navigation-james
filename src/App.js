655297fca2a125e443b1e4d819d0e66f819c46c5
“jameschukwu“ <okoriechukwu@yahoo.com>
2022-04-04 20:20:03 -0500

    updated files

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
-import React from 'react';
-import Style from './App.module.scss';
-import BaseLayout from "./Components/BaseLayout";
-import { HashRouter } from 'react-router-dom';
-import Home from "./Components/Home";
+import React, { useEffect } from "react";
+import Particles from "./components/layouts/Particles";
+import Header from "./components/section/Header";
+import About from "./components/section/About";
+import Works from "./components/section/Works";
+import Contact from "./components/section/Contact";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { animation } from "./profile";
 
 function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: animation.duration,
+      once: animation.once,
+      disable: !animation.animate,
+    });
+    // eslint-disable-next-line
+  }, []);
+
   return (
-      <HashRouter>
-          <div className={Style.app}>
-              <BaseLayout>
-                  <Home />
-              </BaseLayout>
-          </div>
-      </HashRouter>
+    <div className="App">
+      <Header />
+      <Particles />
+      <About />
+      <Works />
+      <Contact />
+    </div>
   );
 }
-
-
 
 export default App;
