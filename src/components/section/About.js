41397e2069da99bd92b5cd753abfc44b9b9cfa51
“jameschukwu“ <okoriechukwu@yahoo.com>
2022-04-04 21:10:08 -0500

    modified files

--- src/components/section/About.js
+++ src/components/section/About.js
@@ -9,7 +9,7 @@ const About = () => {
             <div className="col-12 d-none d-md-block offset-md-1 col-md-4 Photo" id="not-dark2"></div>
             <div className="col-12 offset-md-1 col-md-6 about">
                 <div className="About-title-box">
-                <h1 id="About" className="red-line">{section2title}</h1>
+                <h1 id="About" className="black-line">{section2title}</h1>
                 </div>
                 <p className="lead about-text">
                     {about.paragraph}  
